f1a09b05ecbd6d30bd76075273629cd6d55571a0
José Ernesto Barbosa <jose.barbosa@cwi.com.br>
2019-08-17 14:02:59 -0300

    temporary change

--- controllers/stock.ts
+++ controllers/stock.ts
@@ -30,25 +30,25 @@ router.get('/', (req: Request, res: Response) => {
 
 router.get('/:bookId', (req: Request, res: Response) => {
     let availability = availabilityTable.findOne({ bookId: parseInt(req.params.bookId) });
-    if (!availability) {
-        return res.status(404).send();
-    }
+    // if (!availability) {
+    //     return res.status(404).send();
+    // }
     return res.status(200).send(availability);
 });
 
 router.post('/', (req: Request, res: Response) => {
-    if (availabilityTable.find({ bookId: parseInt(req.body.bookId) }).length > 0) {
-        return res.status(409).send();
-    }
+    // if (availabilityTable.find({ bookId: parseInt(req.body.bookId) }).length > 0) {
+    //     return res.status(409).send();
+    // }
     availabilityTable.insert(req.body);
     return res.status(201).send();
 });
 
 router.put('/', (req: Request, res: Response) => {
     let availability = availabilityTable.findOne({ bookId: parseInt(req.body.bookId) });
-    if (!availability) {
-        return res.status(404).send();
-    }
+    // if (!availability) {
+    //     return res.status(404).send();
+    // }
     availability.stock = req.body.stock;
     availabilityTable.update(availability);
     return res.status(204).send();
@@ -56,27 +56,27 @@ router.put('/', (req: Request, res: Response) => {
 
 router.delete('/:bookId', (req: Request, res: Response) => {
     let availability = availabilityTable.findOne({ bookId: parseInt(req.params.bookId) });
-    if (!availability) {
-        return res.status(404).send();
-    }
+    // if (!availability) {
+    //     return res.status(404).send();
+    // }
     availabilityTable.remove(availability);
     return res.status(200).send();
 });
 
 router.put('/:bookId/devolution', (req: Request, res: Response) => {
     let availability = availabilityTable.findOne({ bookId: parseInt(req.params.bookId) });
-    if (!availability) {
-        return res.status(404).send();
-    }
+    // if (!availability) {
+    //     return res.status(404).send();
+    // }
     availability.stock = availability.stock + 1;
     availabilityTable.update(availability);
     return res.status(204).send();
 });
 router.put('/:bookId/loan', (req: Request, res: Response) => {
     let availability = availabilityTable.findOne({ bookId: parseInt(req.params.bookId) });
-    if (!availability) {
-        return res.status(404).send();
-    }
+    // if (!availability) {
+    //     return res.status(404).send();
+    // }
     let newAvailability = availability.stock - 1;
     if(newAvailability <= 0){
         return res.status(401).send();
